13dc93dc6605a7acd5e8c3acf303041716775d47
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-05-19 20:24:52 +0200

    Add draw_plot() wrapper

---
+++ R/draw_plot.R
@@ -0,0 +1,13 @@
+#' Constructs and plots the A4 growth chart
+#'
+#' This function is a wrapper of \code{groeidiagrammen::draw_plot}
+#' @inheritParams groeidiagrammen::draw_plot
+#' @return A grid object
+#' @export
+draw_plot <- function(ind, chartcode, curve_interpolation = TRUE,
+                      quiet = TRUE) {
+
+  return(groeidiagrammen::draw_plot(ind = ind, chartcode = chartcode,
+                                    curve_interpolation = curve_interpolation,
+                                    quiet = quiet))
+}
