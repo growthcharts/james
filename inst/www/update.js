c4e68b11874f9b888914360adfaca5716cd9bd13
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-06-14 09:47:40 +0200

    Activate chartcode parameter

--- inst/www/update.js
+++ inst/www/update.js
@@ -122,7 +122,7 @@ function update() {
       bds_data : null,
       ind_loc : user_ind,
       selector : selector,
-      chartcode: null,
+      chartcode: user_chartcode,
       chartgrp : chartgrp,
       agegrp   : agegrp,
       sex      : sex,
