e04ae9bb1efab1625de463fb475ec542c98e30eb
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-07-14 10:56:45 +0200

    Extend arguments to match chartplotter::draw_plot()

--- R/draw_plot.R
+++ R/draw_plot.R
@@ -1,14 +1,36 @@
 #' Constructs and plots the A4 growth chart
 #'
-#' This function is a wrapper of \code{groeidiagrammen::draw_plot}
-#' @inheritParams groeidiagrammen::draw_plot
+#' This function is a wrapper of \code{chartplotter::process_chart}
+#' @inheritParams chartplotter::process_chart
 #' @return A grid object
 #' @export
-draw_plot <- function(ind, chartcode, curve_interpolation = TRUE,
-                      quiet = TRUE) {
+draw_plot <- function(individual,
+                      chartcode,
+                      curve_interpolation = TRUE,
+                      quiet = TRUE,
+                      dnr = NULL,
+                      period = numeric(0),
+                      nmatch = 0L,
+                      user_model = 2L,
+                      exact_sex = TRUE,
+                      exact_ga = FALSE,
+                      break_ties = TRUE,
+                      show_realized = FALSE,
+                      show_future = FALSE,
+                      clip = TRUE) {
 
-  return(process_chart(individual = ind,
-                       chartcode = chartcode,
-                       curve_interpolation = curve_interpolation,
-                       quiet = quiet))
+  process_chart(individual = individual,
+                chartcode = chartcode,
+                curve_interpolation = curve_interpolation,
+                quiet = quiet,
+                dnr = dnr,
+                period = period,
+                nmatch = nmatch,
+                user_model = user_model,
+                exact_sex = exact_sex,
+                exact_ga = exact_ga,
+                break_ties = break_ties,
+                show_realized = show_realized,
+                show_future = show_future,
+                clip = clip)
 }
