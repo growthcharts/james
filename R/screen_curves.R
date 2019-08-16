f9b72a12669dea22127cecbd7a7654cecbb7a5dc
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-08-16 11:30:05 +0200

    Change arguments to `txt` and `location`

--- R/screen_curves.R
+++ R/screen_curves.R
@@ -1,6 +1,11 @@
 #' Screen growth curves according to JGZ guidelines
 #'
-#' @inheritParams draw_chart
+#' @inheritParams jsonlite::fromJSON
+#' @param location  A url that points to the server location where the
+#'   parsed data are stored. Optional. If specified, \code{location}
+#'   takes priority over \code{txt}. \code{location} is typically
+#'   the location of a previous \code{OpenCPU} call for uploading
+#'   the data.
 #' @return A JSON string
 #' @examples
 #' \dontrun{
@@ -12,19 +17,18 @@
 #' r1 <- jamesclient::upload_bds(js)
 #' location <- jamesclient::get_url(r1, "location")
 #' location
-#' screen_curves(ind_loc = location)
+#' screen_curves(location = location)
 #'
 #' # implicit upload (slower)
-#' screen_curves(bds_data = js)
+#' screen_curves(txt = js)
 #'}
 #' @export
-screen_curves <- function(bds_data  = NULL,
-                          ind_loc = NULL) {
+screen_curves <- function(txt = NULL, location = NULL) {
 
-  if (length(ind_loc) == 0L) {
-    if (length(bds_data) > 0L) {
-      r1 <- upload_bds(bds_data)
-      ind_loc <- get_url(r1, "location")
+  if (length(location) == 0L) {
+    if (length(txt) > 0L) {
+      r1 <- upload_bds(txt)
+      location <- get_url(r1, "location")
     }
     else {
       return(toJSON(list(
@@ -32,14 +36,14 @@ screen_curves <- function(bds_data  = NULL,
         Resultaten = screen_curves_ind(NULL))))
     }
   }
-  con <- curl(paste0(ind_loc, "R/.val/rda"))
+  con <- curl(paste0(location, "R/.val/rda"))
   load(file = con)
   ind <- .val
   close(con)
   rm(".val")
 
   res <- screen_curves_ind(ind)
-  url <- paste0("https://groeidiagrammen.nl/ocpu/lib/james/www/?ind=", ind_loc)
+  url <- paste0("https://groeidiagrammen.nl/ocpu/lib/james/www/?ind=", location)
   ret <- list(UrlGroeicurven = unbox(url),
               Resultaten = res)
   toJSON(ret)
