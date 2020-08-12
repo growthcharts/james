fe25189537d78ddd6bf3c9a8963350779990ffce
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-08-12 15:24:22 +0200

    custom_list() is a replacement of old sceen_curves() that did too many jobs

---
+++ R/custom_list.R
@@ -0,0 +1,28 @@
+#' Provides a Screen growth curves according to JGZ guidelines
+#'
+#' @inheritParams request_site
+#' @return A table with screening results
+#' @return A list with custom parts
+#' @examples
+#' \dontrun{
+#' fn <- system.file("extdata", "smocc", "Laura_S.json", package = "jamestest")
+#'
+#' # first upload, then create custom list
+#' r1 <- upload_txt(fn)
+#' loc <- jamesclient::get_url(r1, "location")
+#' list1 <- custom_list(loc = loc)
+#'
+#' # upload & screen
+#' list2 <- custom_list(fn)
+#' identical(list1, list2)
+#' }
+#' @export
+custom_list <- function(txt = NULL, loc = NULL) {
+
+  site <- request_site(txt, loc)
+  res <- screen_curves(txt, loc)
+
+  ret <- list(UrlGroeicurven = unbox(site),
+              Resultaten = res)
+  toJSON(ret)
+}
