ab3edc1f83aac951bcad101ada4e6f90623615b6
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-08-13 10:15:58 +0200

    Make validate_chartcode local, returns now TRUE or FALSE

--- R/validate_chartcode.R
+++ R/validate_chartcode.R
@@ -3,15 +3,13 @@
 #' This function checks whether \code{chartcode} is a available
 #' in the chart library.
 #' @param chartcode Chart code, typically something like \code{"NMAB"}
-#' @return The \code{chartcode} if available. \code{NULL} if not
-#' available.
+#' @return \code{TRUE} or \code{FALSE}.
 #' @seealso \code{\link{list_charts}}
 #' @examples
 #' validate_chartcode("NJAA")
-#' @export
 validate_chartcode <- function(chartcode = NULL) {
 
-  if (is.null(chartcode)) return(NULL)
-  if (!chartcode[1L] %in% list_charts()$chartcode) return(NULL)
-  chartcode
+  if (is.null(chartcode)) return(FALSE)
+  chartcode[1L] %in% list_charts()$chartcode
 }
+
