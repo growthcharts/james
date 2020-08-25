659eaed0b42b04f6c92fd8fc5c88afe52d265018
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-08-25 15:04:18 +0200

    Legacy change. The behavior is draw_chart_bds() was altered. Addition of selector default makes it backward compatible.

--- R/draw_chart_bds.R
+++ R/draw_chart_bds.R
@@ -4,22 +4,28 @@
 #' of class \linkS4class{individual}, and then draws the
 #' individual data on the requested growth chart.
 #' @param txt   A JSON string, URL or file
+#' @param selector Legacy addition to solve a problem in jgzApp. See
+#' \code{draw_chart} for interpretation. The default is set to
+#' \code{"chartcode"}.
 #' @param \dots For \code{draw_chart_bds}, additional parameter passed
 #'   down to \code{fromJSON(txt, ...)}, \code{new("xyz",... )} and
 #'   \code{new("bse",... )}. Useful parameters are \code{models =
 #'   "bsmodel"} for setting the broken stick model, or \code{call =
 #'   as.call(...)} for setting proper reference standards.
 #' @inheritParams chartplotter::process_chart
+#' @note DEPRECATED. USE draw_chart INSTEAD.
 #' @examples
 #' fn <- system.file("testdata", "client3.json", package = "james")
 #' g <- draw_chart_bds(txt = fn)
 #' @keywords server
 #' @export
 draw_chart_bds <- function(txt = "", chartcode = "",
-                           curve_interpolation = TRUE, ...) {
+                           curve_interpolation = TRUE,
+                           selector = "chartcode", ...) {
 
   # legacy
   draw_chart(txt = txt, chartcode = chartcode,
-             curve_interpolation = curve_interpolation)
+             curve_interpolation = curve_interpolation,
+             selector = selector)
 }
 
