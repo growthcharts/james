9765fca07fbbb4cf3d4af52c86e6d17fdd410817
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-08-31 20:55:02 +0200

    Add request_site() examples to test

---
+++ tests/testthat/test-request_site.R
@@ -0,0 +1,22 @@
+
+laura <- system.file("testdata", "Laura_S.json", package = "james")
+laura_gro <- system.file("testdata", "Laura_S_gro.json", package = "james")
+laura_dev <- system.file("testdata", "Laura_S_dev.json", package = "james")
+
+kevin <- system.file("testdata", "Kevin_S.json", package = "james")
+kevin_gro <- system.file("testdata", "Kevin_S_gro.json", package = "james")
+kevin_dev <- system.file("testdata", "Kevin_S_dev.json", package = "james")
+
+host1 <- "https://groeidiagrammen.nl"
+host3 <- "http://localhost"
+
+site_laura <- request_site(laura, host = host3)
+site_laura_gro <- request_site(laura_gro, host = host3)
+site_laura_dev <- request_site(laura_dev, host = host3)
+
+site_kevin  <- request_site(kevin, host = host3)
+site_kevin_gro <- request_site(kevin_gro, host = host3)
+site_kevin_dev <- request_site(kevin_dev, host = host3)
+
+
+# browseURL(site_kevin_dev)
