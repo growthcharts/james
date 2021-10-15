bcc7172bffc88f173956d668a3c6295eb4a92fe5
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-10-15 14:29:49 +0200

    Add authenticate() function

--- R/authenticate.R
+++ R/authenticate.R
@@ -1,10 +1,11 @@
 #' Authentication request
 #'
-#' The function `authenticate_jwt` decrypts a JSON webtoken using a public key.
+#' The function `authenticate()` decrypts a JSON webtoken using a public key.
 #' Returns `TRUE` if the request is granted to JAMES.
 #'
 #' @rdname authenticate
 #' @inheritParams jose::jwt_encode_sig
+#' @param \dots Not used
 #' @return A boolean
 #' @author Arjan Huizing, Stef van Buuren 2021
 #' @keywords authentication
@@ -13,14 +14,20 @@
 #' pubkey <- as.list(key)$pubkey
 #' claim <- jose::jwt_claim(user = "test", session_key = 123, applications = "james;srm;psycat")
 #' jwt <- jose::jwt_encode_sig(claim, key = key)
-#' james:::authenticate_jwt(jwt, pubkey)
+#' james:::authenticate(jwt, pubkey)
+authenticate <- function(jwt = NULL, pubkey = NULL, ...) {
+  if (!authenticate_jwt(jwt = jwt, pubkey = pubkey)) {
+    stop("JAMES: No authorisation.")
+  }
+  return(invisible(TRUE))
+}
+
 authenticate_jwt <- function(jwt = NULL, pubkey = NULL) {
   if (!authenticate_status()) {
     return(TRUE)
   }
   if (is.null(jwt)) stop("JAMES: No token found.")
   if (is.null(pubkey)) stop("JAMES: No public key found.")
-
   parsed_claim <- jwt_decode_sig(jwt, pubkey)
   apps <- unlist(strsplit(parsed_claim$applications, split = ";"))
   return("james" %in% apps)
@@ -28,7 +35,7 @@ authenticate_jwt <- function(jwt = NULL, pubkey = NULL) {
 
 authenticate_status <- function(x = NA) {
   if (is.na(x)) {
-    return(ifelse(Sys.getenv("JWT_AUTH") == "n", FALSE, TRUE))
+    return(invisible(ifelse(Sys.getenv("JWT_AUTH") == "n", FALSE, TRUE)))
   }
   Sys.setenv("JWT_AUTH" = ifelse(x, "y", "n"))
   authenticate_status()
