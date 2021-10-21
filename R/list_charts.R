3adf23eed4a66c22061001368ea0b0256482969b
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-10-21 14:24:41 +0200

    Add explicit authToken argument

--- R/list_charts.R
+++ R/list_charts.R
@@ -1,9 +1,10 @@
 #' List the available growth charts
 #'
+#' @param authToken A JSON web token.
 #' @param \dots Used for authentication
 #'
 #' @export
-list_charts <- function(...) {
-  authenticate(...)
+list_charts <- function(authToken = NULL, ...) {
+  authenticate(authToken = authToken, ...)
   chartbox::list_charts()
 }
