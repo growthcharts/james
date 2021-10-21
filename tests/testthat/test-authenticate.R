88898664bf36a196b9e85c9f558c5c5d81b54816
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-10-21 13:42:32 +0200

    Tweak to no_auth version

--- tests/testthat/test-authenticate.R
+++ tests/testthat/test-authenticate.R
@@ -6,6 +6,6 @@ claim <- jose::jwt_claim(data = list(applications = "james;srm;psycat"))
 jwt <- jose::jwt_encode_sig(claim, key = key)
 
 test_that("validates with local key", {
-  expect_true(james:::authenticate_status())
+  expect_equal(james:::authenticate_status(), Sys.getenv("JWT_AUTH") == "y")
   expect_true(james:::authenticate(jwt, pubkey))
 })
