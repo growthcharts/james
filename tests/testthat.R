66cc4bbc821669035c9d6833f97b54bcd9824343
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-10-21 13:30:33 +0200

    Add skip_if_auth() function for testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,11 @@
 library(testthat)
 library(james)
 
+skip_if_auth <- function() {
+  if (identical(Sys.getenv("JWT_AUTH"), "n")) {
+    return(invisible(TRUE))
+  }
+  skip("Not run when authentication is on")
+}
+
 test_check("james")
