c92255ef733f8dc9aec2064b25de48b43a91b7c0
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-12-21 10:19:53 +0100

    Make function arguments of screening functions visible in james

--- R/list_screeners.R
+++ R/list_screeners.R
@@ -1,10 +1,13 @@
 #' List the available growth screeners
 #'
+#' @param ynames Character vector identifying the measures to be
+#' screened. By default, `ynames = c("hgt", "wgt", "hdc")`.
 #' @param \dots Used for authentication, passed down to [growthscreener::list_screeners()].
 #' @examples
 #' head(list_screeners(ynames = "hgt"), 2)
 #' @export
-list_screeners <- function(...) {
+list_screeners <- function(ynames = c("hgt", "wgt", "hdc"),
+                           ...) {
   authenticate(...)
-  growthscreener::list_screeners(...)
+  growthscreener::list_screeners(ynames = ynames, ...)
 }
--- R/screen_growth.R
+++ R/screen_growth.R
@@ -1,7 +1,12 @@
 #' Screen growth curves according to JGZ guidelines
 #'
-#' @inheritParams request_site
 #' @inheritParams bdsreader::read_bds
+#' @inheritParams request_site
+#' @param ynames Character vector identifying the measures to be
+#' screened. By default, `ynames = c("hgt", "wgt", "hdc")`.
+#' @param na.omit A logical indicating whether records with a missing
+#' `x` (age) or `y` (yname) should be removed. Defaults to
+#' `TRUE`.
 #' @note `screen_growth` superseeds `screen_curves` and will
 #' only return results from growth screening.
 #' @examples
@@ -19,7 +24,15 @@
 #' screen_growth(fn)
 #' }
 #' @export
-screen_growth <- function(txt = "", loc = "", format = "1.0", ...) {
+screen_growth <- function(txt = "",
+                          loc = "",
+                          format = "1.0",
+                          ynames = c("hgt", "wgt", "hdc"),
+                          na.omit = TRUE,
+                          ...) {
   authenticate(...)
-  screen_curves_ind(ind = get_tgt(txt, loc, format = format), ...)
+  screen_curves_ind(ind = get_tgt(txt, loc, format = format),
+                    ynames = ynames,
+                    na.omit = na.omit,
+                    ...)
 }
