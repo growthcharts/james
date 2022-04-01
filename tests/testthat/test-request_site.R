814f36b361a23cdbc79a100218f32091c91c5ef1
Stef van Buuren <stef.vanbuuren@tno.nl>
2022-04-01 15:21:06 +0200

    Make execution of examples and tests conditional on valid_url()

--- tests/testthat/test-request_site.R
+++ tests/testthat/test-request_site.R
@@ -9,20 +9,23 @@ kevin_dev <- system.file("testdata", "Kevin_S_dev.json", package = "james")
 
 host <- "http://localhost"
 
-test_that(
-  "creates site_laura",
-  expect_silent(site_laura <- request_site(laura, host = host))
-)
+if (jamesclient::valid_url(host)) {
+  test_that(
+    "creates site_laura",
+    expect_silent(site_laura <- request_site(laura, host = host))
+  )
 
-#site_laura_gro <- request_site(laura_gro, host = host)
-#site_laura_dev <- request_site(laura_dev, host = host)
-#site_laura_dev_2 <- request_site(laura_dev_2, host = host)
+  #site_laura_gro <- request_site(laura_gro, host = host)
+  #site_laura_dev <- request_site(laura_dev, host = host)
+  #site_laura_dev_2 <- request_site(laura_dev_2, host = host)
 
-test_that(
-  "creates site_kevin",
-  expect_silent(site_kevin <- request_site(kevin, host = host))
-)
-# site_kevin_gro <- request_site(kevin_gro, host = host)
-# site_kevin_dev <- request_site(kevin_dev, host = host)
+  test_that(
+    "creates site_kevin",
+    expect_silent(site_kevin <- request_site(kevin, host = host))
+  )
 
-# browseURL(site_laura)
+  # site_kevin_gro <- request_site(kevin_gro, host = host)
+  # site_kevin_dev <- request_site(kevin_dev, host = host)
+
+  # browseURL(site_laura)
+}
--- tests/testthat/test-screen_growth.R
+++ tests/testthat/test-screen_growth.R
@@ -7,32 +7,33 @@ fn <- system.file("extdata", "allegrosultum", "client3.json", package = "jamesde
 js <- jsonlite::toJSON(jsonlite::fromJSON(fn), auto_unbox = TRUE)
 host <- "http://localhost"
 
-path <- "ocpu/library/james/R/upload_data"
-url <- modify_url(url = host, path = path)
-resp <- POST(
-  url = url,
-  body = list(txt = js),
-  encode = "json",
-  add_headers(Accept = "plain/text")
-)
-test_that(
-  "file client3.json uploads to server",
-  expect_equal(status_code(resp), 201)
-)
+if (jamesclient::valid_url(host)) {
+  path <- "ocpu/library/james/R/upload_data"
+  url <- modify_url(url = host, path = path)
+  resp <- POST(
+    url = url,
+    body = list(txt = js),
+    encode = "json",
+    add_headers(Accept = "plain/text")
+  )
+  test_that(
+    "file client3.json uploads to server",
+    expect_equal(status_code(resp), 201)
+  )
 
-path <- "ocpu/library/james/R/screen_growth"
-url <- modify_url(url = host, path = path)
-resp <- POST(
-  url = url,
-  body = list(txt = js),
-  encode = "json",
-  add_headers(Accept = "plain/text")
-)
-test_that(
-  "file client3.json is screened",
-  expect_equal(status_code(resp), 201)
-)
-
+  path <- "ocpu/library/james/R/screen_growth"
+  url <- modify_url(url = host, path = path)
+  resp <- POST(
+    url = url,
+    body = list(txt = js),
+    encode = "json",
+    add_headers(Accept = "plain/text")
+  )
+  test_that(
+    "file client3.json is screened",
+    expect_equal(status_code(resp), 201)
+  )
+}
 
 # Allegro Sultum 1 sept 2020
 laura_dev <- system.file("testdata", "Laura_S_dev.json", package = "james")
