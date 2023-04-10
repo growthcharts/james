0150f9952a2c56caa6e69f7362717db8b7a9b603
Stef van Buuren <stef.vanbuuren@tno.nl>
2023-04-10 23:02:07 +0200

    Make transparent widget background

--- R/make_sds_chart.R
+++ R/make_sds_chart.R
@@ -2,22 +2,24 @@
 #'
 #' Save the plot as `sds.html` and an external library `lib2`
 #' @param title Widget title
-#' @param background Widget background
 #' @export
-make_sds_chart <- function(title = "SDS plot", background = "#F7F7F7") {
+make_sds_chart <- function(title = "SDS plot") {
   trace_0 <- stats::rnorm(100, mean = 5)
   trace_1 <- stats::rnorm(100, mean = 0)
-  trace_2 <- stats::rnorm(100, mean = -5)
   x <- c(1:100)
 
   data <- data.frame(x, trace_0, trace_1, trace_2)
 
-  fig <- plot_ly(data, x = ~x, y = ~trace_0, name = 'trace 0', type = 'scatter', mode = 'lines')
-  fig <- fig %>% add_trace(y = ~trace_1, name = 'trace 1', mode = 'lines+markers')
-  fig <- fig %>% add_trace(y = ~trace_2, name = 'trace 2', mode = 'markers')
+  fig <- plot_ly(data, x = ~x, y = ~trace_0, name = 'trace 0',
+                 type = 'scatter', mode = 'lines')
+  fig <- fig %>%
+    add_trace(y = ~trace_1, name = 'trace 1', mode = 'lines+markers') %>%
+    layout(title = "Standard Deviation Scores Chart",
+           plot_bgcolor = "rgba(0,0,0,0)",
+           paper_bgcolor = "rgba(0,0,0,0)")
 
   htmlwidgets::saveWidget(widget = fig, file = "sds.html",
                           selfcontained = FALSE, libdir = "lib2",
-                          background = background, title = title)
+                          title = title)
   invisible(NULL)
 }
