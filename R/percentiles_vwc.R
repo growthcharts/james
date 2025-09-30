a40d0c7503874d347b3c6749a0cc39945b2a49d9
Huizing <arjan.huizing@tno.nl>
2025-09-30 15:37:42 +0200

    Separate select and percentiles for vwc

--- R/percentiles_vwc.R
+++ R/percentiles_vwc.R
@@ -4,8 +4,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#' Calculates the recommended van Wiechen milestones based on age and past
-#' milestones
+#' Provides additional information on percentiles of D-score and age in months
+#' for the percentiles 2, 10, 50, 90 and 98. User either provides known van
+#' Wiechen milestones, or information to calculate the recommended van Wiechen
+#' milestones based on age and past milestones.
 #'
 #' @inheritParams calculate_dscore
 #' @param p Reference percentile indicating the expected probability of a
@@ -13,16 +15,19 @@
 #'   easier items.
 #' @param n Number of van Wiechen items to suggest. By default returns all items
 #'   within set probability limits.
+#' @param vwc Vector of string values matching van Wiechen milestones. Bypasses
+#'   calculations and directly supply the milestones of interest
 #' @inheritParams bdsreader::read_bds
-#' @return A string of milestones
+#' @return A table.
 #' @author Arjan Huizing 2025
 #' @examples
 #' fn <- system.file("testdata", "Laura_S.json", package = "james")
-#' select_vwc(txt = fn, p = 50, n = 10)
+#' percentiles_vwc(txt = fn, p = 50, n = 3)
 #' @export
-select_vwc <- function(txt = "",
+percentiles_vwc <- function(txt = "",
                        p = 90,
                        n = 6,
+                       vwc = NULL,
                        session = "",
                        format = "1.0",
                        loc = "",
@@ -30,6 +35,9 @@ select_vwc <- function(txt = "",
                        ...) {
   authenticate(...)
   daz <- d <- NULL
+
+  if (!is.null(vwc)) return(vwc::vwc_percentiles(vwc))
+
 
   if (!missing(loc)) {
     warning("Argument loc is deprecated and will disappear in Nov 2022; please use session instead.",
@@ -49,18 +57,18 @@ select_vwc <- function(txt = "",
   }
   time <- timedata(tgt)
 
-if ("dsc" %in% time[time$age == max(time$age), "yname"]) {
-  d <- unlist(time[time$age == max(time$age) & time$yname == "dsc", "y"])
-  # Confirm with Iris if this adds anything:
-  # daz <- unlist(time[time$age == max(time$age) & time$yname == "dsc", "z"])
-}
+  if ("dsc" %in% time[time$age == max(time$age), "yname"]) {
+    d <- unlist(time[time$age == max(time$age) & time$yname == "dsc", "y"])
+    # Confirm with Iris if this adds anything:
+    # daz <- unlist(time[time$age == max(time$age) & time$yname == "dsc", "z"])
+  }
 
   vwc <- vwc::select_vwc(age = max(time$age),
-                    p = p,
-                    d = d,
-                    daz = daz,
-                    n = n,
-                    passed_items = time[time$y == 1.00, ]$yname)
+                         p = p,
+                         d = d,
+                         daz = daz,
+                         n = n,
+                         passed_items = time[time$y == 1.00, ]$yname)
 
-  return(paste(unique(vwc), collapse = "\n"))
+  return(vwc::vwc_percentiles(vwc))
 }
