9524b968d6630897d5a73dcb008b1f5291bbf75b
Stef van Buuren <stef.vanbuuren@tno.nl>
2025-11-23 21:44:50 +0100

    Inactive configuration tests

--- tests/testthat/test-request_site.R
+++ tests/testthat/test-request_site.R
@@ -9,33 +9,14 @@ kevin_dev <- system.file("testdata", "Kevin_S_dev.json", package = "james")
 
 host <- "http://localhost:8080"
 
-if (jamesclient::valid_url(host)) {
-  test_that(
-    "creates site_laura",
-    expect_true(jamesclient::valid_url(request_site(
-      txt = laura,
-      sitehost = host
-    )))
-  )
 
-  #site_laura_gro <- request_site(laura_gro, sitehost = host)
-  #site_laura_dev <- request_site(laura_dev, sitehost = host)
-  #site_laura_dev_2 <- request_site(laura_dev_2, sitehost = host)
+#site_laura_gro <- request_site(laura_gro, sitehost = host)
+#site_laura_dev <- request_site(laura_dev, sitehost = host)
+#site_laura_dev_2 <- request_site(laura_dev_2, sitehost = host)
+# site_kevin_gro <- request_site(kevin_gro, sitehost = host)
+# site_kevin_dev <- request_site(kevin_dev, sitehost = host)
 
-  test_that(
-    "creates site_kevin",
-    expect_true(jamesclient::valid_url(request_site(
-      txt = kevin,
-      sitehost = host
-    )))
-  )
-
-  # site_kevin_gro <- request_site(kevin_gro, sitehost = host)
-  # site_kevin_dev <- request_site(kevin_dev, sitehost = host)
-
-  # browseURL(site_laura)
-}
-
+# browseURL(site_laura)
 
 ## NESTED UPLOADS FAIL ON LOCALHOST
 ## The site/request/json endpoint does not work with nested uploads on localhost.
@@ -48,18 +29,18 @@ library(jamesclient)
 fn <- system.file("examples/maria.json", package = "bdsreader")
 js <- read_json_js(fn)
 
-test_that("request_site WORKS with nested upload for proper domains", {
-  host <- "https://james.groeidiagrammen.nl"
-  r <- james_post(
-    host = host,
-    path = "/site/request/json",
-    txt = js,
-    sitehost = host
-  )
-  expect_equal(status_code(r), 201L)
-  site <- r$parsed
-  # browseURL(site)
-})
+# test_that("request_site WORKS with nested upload for proper domains", {
+#   host <- "https://james.groeidiagrammen.nl"
+#   r <- james_post(
+#     host = host,
+#     path = "/site/request/json",
+#     txt = js,
+#     sitehost = host
+#   )
+#   expect_equal(status_code(r), 201L)
+#   site <- r$parsed
+#   # browseURL(site)
+# })
 
 test_that("request_site FAILS with nested upload on localhost", {
   host <- "http://localhost:8080"
@@ -81,14 +62,14 @@ test_that("request_site can be replaced by data/upload to localhost", {
   # browseURL(site)
 })
 
-test_that("site creation by data/upload also works on proper domains", {
-  host <- "https://james.groeidiagrammen.nl"
-  r <- james_post(host = host, path = "data/upload", txt = js)
-  expect_equal(status_code(r), 201L)
-  session <- get_url(r, "session")
-  site <- modify_url(host, path = "site", query = list(session = session))
-  # browseURL(site)
-})
+# test_that("site creation by data/upload also works on proper domains", {
+#   host <- "https://james.groeidiagrammen.nl"
+#   r <- james_post(host = host, path = "data/upload", txt = js)
+#   expect_equal(status_code(r), 201L)
+#   session <- get_url(r, "session")
+#   site <- modify_url(host, path = "site", query = list(session = session))
+#   # browseURL(site)
+# })
 
 test_that("detect_ocpu_host works", {
   expect_identical(
@@ -107,20 +88,20 @@ test_that("detect_ocpu_host works", {
   )
 })
 
-test_that("request_site works in dual-mode", {
-  js <- "{\"Format\":\"3.0\",\"reference\":\"Test\"}"
+# test_that("request_site works in dual-mode", {
+#   js <- "{\"Format\":\"3.0\",\"reference\":\"Test\"}"
 
-  local_host <- "http://localhost:8080"
-  prod_host <- "https://james.groeidiagrammen.nl"
+#   local_host <- "http://localhost:8080"
+#   prod_host <- "https://james.groeidiagrammen.nl"
 
-  # Local session URL begins with localhost
-  url_loc <- request_site(txt = js, sitehost = local_host)
-  expect_true(grepl("^http://localhost:8080/site\\?session=", url_loc))
+#   # Local session URL begins with localhost
+#   url_loc <- request_site(txt = js, sitehost = local_host)
+#   expect_true(grepl("^http://localhost:8080/site\\?session=", url_loc))
 
-  # Production URL begins with https://james...
-  url_prod <- request_site(txt = js, sitehost = prod_host)
-  expect_true(grepl(
-    "^https://james.groeidiagrammen.nl/site\\?session=",
-    url_prod
-  ))
-})
+#   # Production URL begins with https://james...
+#   url_prod <- request_site(txt = js, sitehost = prod_host)
+#   expect_true(grepl(
+#     "^https://james.groeidiagrammen.nl/site\\?session=",
+#     url_prod
+#   ))
+# })
