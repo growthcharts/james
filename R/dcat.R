c409aeebe6679e09b54494b9c29a8b5c66e361e1
Iris Eekhout <iriseekhout@gmail.com>
2025-12-16 13:45:24 +0100

    Remove selection for max age in responses for james/dcat; was bugged - selected only one row and selection is not necessary.

--- R/dcat.R
+++ R/dcat.R
@@ -17,6 +17,8 @@
 #' @examples
 #' txt <- system.file("examples", "example_v3.1.json", package = "bdsreader")
 #' dcat(txt = txt, p = 50)
+#' txt <- "~/OneDrive - TNO/Documents/GitHub/james/data-raw/test_data.json"
+#' dcat(txt = txt, p = 50)
 #' @export
 dcat <- function(
   txt = "",
@@ -61,7 +63,6 @@ dcat <- function(
   time <- timedata(tgt)
   # extract most recent administered items
   if (nrow(time) > 0) {
-    dat <- time[which.max(time$age), ]
     colnames(dat)[colnames(dat) %in% c("yname", "y")] <- c("item", "score")
   }
 
